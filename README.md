e7d8c99776ad7b310cc35ef156fd58fab2ed990d
Normandes Junior <normandesjr@gmail.com>
2017-05-06 09:23:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# demo-oauth2-spring-security
+A simple demo of Password Flow with Spring Security OAuth 2
---
+++ src/main/java/com/example/DemoOauth2SpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOauth2SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOauth2SpringSecurityApplication.class, args);
+	}
+}
